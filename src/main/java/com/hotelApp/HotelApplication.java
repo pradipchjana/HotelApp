261a837a7c69c63951609393c70e5d1e36706e07
Pradip Jana <janaprdip501@gmail.com>
2026-05-21 11:52:13 +0530

    Creates spring app and run on port 8080

---
+++ src/main/java/com/hotelApp/HotelApplication.java
@@ -0,0 +1,11 @@
+package com.hotelApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApplication {
+    static void main(String[] args) {
+        SpringApplication.run(HotelApplication.class, args);
+    }
+}
