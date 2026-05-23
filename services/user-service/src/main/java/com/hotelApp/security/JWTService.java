a88fa83dc369a15cb14571435b0336c091ab9b66
Pradip Jana <janaprdip501@gmail.com>
2026-05-23 10:59:55 +0530

    pull jwt key in env

--- services/user-service/src/main/java/com/hotelApp/security/JWTService.java
+++ services/user-service/src/main/java/com/hotelApp/security/JWTService.java
@@ -4,34 +4,45 @@ import com.hotelApp.user.modal.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Service
 public class JWTService {
-  private final String secret = "ourhotelappmadebykhasimjanadinesh";
-  private final Key key = Keys.hmacShaKeyFor(secret.getBytes());
 
-  public String generateToken(User user){
-    int expirationTime = 1000 * 60 * 60;
+  private final SecretKey key;
+
+  public JWTService(@Value("${jwt.secret}") String secret) {
+    this.key = Keys.hmacShaKeyFor(secret.getBytes());
+  }
+
+  public String generateToken(User user) {
+
+    long expirationTime = 1000 * 60 * 60;
 
     return Jwts.builder()
             .subject(user.getUsername())
             .issuedAt(new Date())
-            .expiration(new Date(System.currentTimeMillis() * expirationTime))
+            .expiration(new Date(System.currentTimeMillis() + expirationTime))
             .signWith(key)
             .compact();
   }
 
-  public String extractUsername(String token){
-    Claims claims = Jwts.parser().verifyWith((javax.crypto.SecretKey) key).build().parseSignedClaims(token).getPayload();
+  public String extractUsername(String token) {
+
+    Claims claims = Jwts.parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
 
     return claims.getSubject();
   }
 
-  public boolean isValid(String token,String username){
-    return  extractUsername(token).equals(username);
+  public boolean isValid(String token, String username) {
+    return extractUsername(token).equals(username);
   }
 }
